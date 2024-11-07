850a62b19aa58d8cae6fac72c7cabb4623cdaf98
Jeff Disher <jeff@jeffdisher.com>
2024-11-07 17:06:33 -0500

    Fixed a bug confusing attribute index/location
    -this problem was exposed by some other shader experiments but we do only want to use the "index" in the case where we are introspecting the attribute, and the "location" in all other cases (this was previously assuming that the "location" was being used in all cases)

--- core/src/main/java/com/jeffdisher/october/peaks/graphics/Program.java
+++ core/src/main/java/com/jeffdisher/october/peaks/graphics/Program.java
@@ -33,19 +33,40 @@ public class Program
 		}
 		gl.glLinkProgram(program);
 		
+		// Verify that the attribute locations are what we requested.
+		for (int index = 0; index < attributesInOrder.length; ++index)
+		{
+			int loc = gl.glGetAttribLocation(program, attributesInOrder[index]);
+			Assert.assertTrue(index == loc);
+		}
+		
 		// We now want to extract the attribute data.
+		// Note that glGetActiveAttrib takes "attribute index" while glGetAttribLocation takes "attribute location" (set
+		// by glBindAttribLocation) so we will need to use the name to find the type information, not the location.
+		// See this for more information:  https://www.devgem.io/posts/understanding-glgetattriblocation-and-glgetactiveattrib-in-opengl
 		IntBuffer outSize = _allocOutParam();
 		IntBuffer outType = _allocOutParam();
+		
+		// First, find out the attribute indices.
+		gl.glGetProgramiv(program, GL20.GL_ACTIVE_ATTRIBUTES, outSize);
+		int attributeCount = outSize.get(0);
+		Assert.assertTrue(attributeCount == attributesInOrder.length);
+		
+		// Now, walk these to find the name in order to get the type/size.
 		Attribute[] attributes = new Attribute[attributesInOrder.length];
-		for (int index = 0; index < attributesInOrder.length; ++index)
+		for (int index = 0; index < attributeCount; ++index)
 		{
 			gl.glEnableVertexAttribArray(index);
 			String name = gl.glGetActiveAttrib(program, index, outSize, outType);
 			Assert.assertTrue(GL20.GL_NO_ERROR == gl.glGetError());
+			
 			// NOTE:  These IntBuffer out-params are directly written so we don't clear/flip like a normal Buffer type.
 			int floatsInType;
 			switch (outType.get(0))
 			{
+			case GL20.GL_FLOAT:
+				floatsInType = 1;
+				break;
 			case GL20.GL_FLOAT_VEC2:
 				floatsInType = 2;
 				break;
@@ -57,7 +78,19 @@ public class Program
 					throw Assert.unreachable();
 			}
 			int floatsInAttribute = outSize.get(0) * floatsInType;
-			attributes[index] = new Attribute(name, floatsInAttribute);
+			
+			// Find the stored index and write the attribute (this is n^2 but on a very small list and only once).
+			int storedIndex = -1;
+			for (int i = 0; (-1 == storedIndex) && (i < attributesInOrder.length); ++i)
+			{
+				if (name.equals(attributesInOrder[i]))
+				{
+					storedIndex = i;
+				}
+			}
+			Assert.assertTrue(storedIndex >= 0);
+			Assert.assertTrue(null == attributes[storedIndex]);
+			attributes[storedIndex] = new Attribute(name, floatsInAttribute);
 		}
 		Assert.assertTrue(GL20.GL_NO_ERROR == gl.glGetError());
 		return new Program(gl, program, attributes);
