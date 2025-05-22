6264646fb9ac779d54b321d6160f303d2e293548
Jeff Disher <jeff@jeffdisher.com>
2025-05-22 02:29:23 -0400

    Fixed a bug where failed connection would cause hang on shutdown
    -this hang was caused by an orpahned "Cuboid Mesh Baking" thread owned by the SceneRenderer whose shutdown wasn't being hit when the ConnectException was thrown

--- core/src/main/java/com/jeffdisher/october/peaks/GameSession.java
+++ core/src/main/java/com/jeffdisher/october/peaks/GameSession.java
@@ -74,108 +74,21 @@ public class GameSession
 		Map<Block, Prism> specialBlockBounds = this.scene.getModelBoundingBoxes();
 		this.selectionManager = new SelectionManager(environment, specialBlockBounds, this.blockLookup);
 		
-		this.client = new ClientWrapper(environment
-				, new ClientWrapper.IUpdateConsumer() {
-					@Override
-					public void loadNew(IReadOnlyCuboidData cuboid, ColumnHeightMap heightMap)
-					{
-						GameSession.this.cuboids.put(cuboid.getCuboidAddress(), cuboid);
-						GameSession.this.scene.setCuboid(cuboid, heightMap, null);
-					}
-					@Override
-					public void updateExisting(IReadOnlyCuboidData cuboid, ColumnHeightMap heightMap, Set<BlockAddress> changedBlocks)
-					{
-						GameSession.this.cuboids.put(cuboid.getCuboidAddress(), cuboid);
-						GameSession.this.scene.setCuboid(cuboid, heightMap, changedBlocks);
-					}
-					@Override
-					public void unload(CuboidAddress address)
-					{
-						IReadOnlyCuboidData removed = GameSession.this.cuboids.remove(address);
-						Assert.assertTrue(null != removed);
-						GameSession.this.scene.removeCuboid(address);
-					}
-					@Override
-					public void thisEntityUpdated(Entity authoritativeEntity, Entity projectedEntity)
-					{
-						EntityLocation eyeLocation = SpatialHelpers.getEyeLocation(MutableEntity.existing(projectedEntity));
-						GameSession.this.movement.setEye(Vector.fromEntityLocation(eyeLocation));
-						Vector eye = GameSession.this.movement.computeEye();
-						Vector target = GameSession.this.movement.computeTarget();
-						Vector upVector = GameSession.this.movement.computeUpVector();
-						GameSession.this.scene.updatePosition(eye, target, upVector);
-						GameSession.this.selectionManager.updatePosition(eye, target);
-						GameSession.this.selectionManager.setThisEntity(projectedEntity);
-						GameSession.this.eyeEffect.setThisEntity(projectedEntity);
-						GameSession.this.audioManager.setThisEntity(authoritativeEntity, projectedEntity);
-						_callouts.thisEntityUpdated(projectedEntity);
-					}
-					@Override
-					public void thisEntityHurt()
-					{
-						GameSession.this.eyeEffect.thisEntityHurt();
-						GameSession.this.audioManager.thisEntityHurt();
-					}
-					@Override
-					public void otherClientJoined(int clientId, String name)
-					{
-						_callouts.otherClientJoined(clientId, name);
-					}
-					@Override
-					public void otherClientLeft(int clientId)
-					{
-						_callouts.otherClientLeft(clientId);
-					}
-					@Override
-					public void otherEntityUpdated(PartialEntity entity)
-					{
-						GameSession.this.scene.setEntity(entity);
-						GameSession.this.selectionManager.setEntity(entity);
-						GameSession.this.audioManager.setOtherEntity(entity);
-					}
-					@Override
-					public void otherEntityDidUnload(int id)
-					{
-						GameSession.this.scene.removeEntity(id);
-						GameSession.this.selectionManager.removeEntity(id);
-						GameSession.this.audioManager.removeOtherEntity(id);
-					}
-					@Override
-					public void otherEntityHurt(int id, AbsoluteLocation location)
-					{
-						GameSession.this.scene.entityHurt(id);
-						GameSession.this.audioManager.otherEntityHurt(location, id);
-					}
-					@Override
-					public void otherEntityKilled(int id, AbsoluteLocation location)
-					{
-						// NOTE:  ServerStateManager will always notify us of entity death, but only sends the location if we have seen them (to notify us that a player died, for example).
-						if (null != location)
-						{
-							GameSession.this.audioManager.otherEntityKilled(location, id);
-						}
-					}
-					@Override
-					public void tickDidComplete(long gameTick, float skyLightMultiplier, float dayProgression)
-					{
-						GameSession.this.scene.setDayTime(dayProgression, skyLightMultiplier);
-						GameSession.this.audioManager.tickCompleted();
-					}
-					@Override
-					public void blockPlaced(AbsoluteLocation location)
-					{
-						GameSession.this.audioManager.blockPlaced(location);
-					}
-					@Override
-					public void blockBroken(AbsoluteLocation location)
-					{
-						GameSession.this.audioManager.blockBroken(location);
-					}
-				}
+		try
+		{
+			this.client = new ClientWrapper(environment
+				, new _UpdateConsumer()
 				, clientName
 				, serverSocketAddress
 				, localWorldDirectory
-		);
+			);
+		}
+		catch (ConnectException e)
+		{
+			// Shut down the scene since we are going to fail in this start-up.
+			this.scene.shutdown();
+			throw e;
+		}
 		
 		// Load the audio.
 		this.audioManager = new AudioManager(environment, resources);
@@ -207,5 +120,105 @@ public class GameSession
 		void thisEntityUpdated(Entity projectedEntity);
 		void otherClientJoined(int clientId, String name);
 		void otherClientLeft(int clientId);
+	}
+
+
+	private class _UpdateConsumer implements ClientWrapper.IUpdateConsumer
+	{
+		@Override
+		public void loadNew(IReadOnlyCuboidData cuboid, ColumnHeightMap heightMap)
+		{
+			GameSession.this.cuboids.put(cuboid.getCuboidAddress(), cuboid);
+			GameSession.this.scene.setCuboid(cuboid, heightMap, null);
+		}
+		@Override
+		public void updateExisting(IReadOnlyCuboidData cuboid, ColumnHeightMap heightMap, Set<BlockAddress> changedBlocks)
+		{
+			GameSession.this.cuboids.put(cuboid.getCuboidAddress(), cuboid);
+			GameSession.this.scene.setCuboid(cuboid, heightMap, changedBlocks);
+		}
+		@Override
+		public void unload(CuboidAddress address)
+		{
+			IReadOnlyCuboidData removed = GameSession.this.cuboids.remove(address);
+			Assert.assertTrue(null != removed);
+			GameSession.this.scene.removeCuboid(address);
+		}
+		@Override
+		public void thisEntityUpdated(Entity authoritativeEntity, Entity projectedEntity)
+		{
+			EntityLocation eyeLocation = SpatialHelpers.getEyeLocation(MutableEntity.existing(projectedEntity));
+			GameSession.this.movement.setEye(Vector.fromEntityLocation(eyeLocation));
+			Vector eye = GameSession.this.movement.computeEye();
+			Vector target = GameSession.this.movement.computeTarget();
+			Vector upVector = GameSession.this.movement.computeUpVector();
+			GameSession.this.scene.updatePosition(eye, target, upVector);
+			GameSession.this.selectionManager.updatePosition(eye, target);
+			GameSession.this.selectionManager.setThisEntity(projectedEntity);
+			GameSession.this.eyeEffect.setThisEntity(projectedEntity);
+			GameSession.this.audioManager.setThisEntity(authoritativeEntity, projectedEntity);
+			_callouts.thisEntityUpdated(projectedEntity);
+		}
+		@Override
+		public void thisEntityHurt()
+		{
+			GameSession.this.eyeEffect.thisEntityHurt();
+			GameSession.this.audioManager.thisEntityHurt();
+		}
+		@Override
+		public void otherClientJoined(int clientId, String name)
+		{
+			_callouts.otherClientJoined(clientId, name);
+		}
+		@Override
+		public void otherClientLeft(int clientId)
+		{
+			_callouts.otherClientLeft(clientId);
+		}
+		@Override
+		public void otherEntityUpdated(PartialEntity entity)
+		{
+			GameSession.this.scene.setEntity(entity);
+			GameSession.this.selectionManager.setEntity(entity);
+			GameSession.this.audioManager.setOtherEntity(entity);
+		}
+		@Override
+		public void otherEntityDidUnload(int id)
+		{
+			GameSession.this.scene.removeEntity(id);
+			GameSession.this.selectionManager.removeEntity(id);
+			GameSession.this.audioManager.removeOtherEntity(id);
+		}
+		@Override
+		public void otherEntityHurt(int id, AbsoluteLocation location)
+		{
+			GameSession.this.scene.entityHurt(id);
+			GameSession.this.audioManager.otherEntityHurt(location, id);
+		}
+		@Override
+		public void otherEntityKilled(int id, AbsoluteLocation location)
+		{
+			// NOTE:  ServerStateManager will always notify us of entity death, but only sends the location if we have seen them (to notify us that a player died, for example).
+			if (null != location)
+			{
+				GameSession.this.audioManager.otherEntityKilled(location, id);
+			}
+		}
+		@Override
+		public void tickDidComplete(long gameTick, float skyLightMultiplier, float dayProgression)
+		{
+			GameSession.this.scene.setDayTime(dayProgression, skyLightMultiplier);
+			GameSession.this.audioManager.tickCompleted();
+		}
+		@Override
+		public void blockPlaced(AbsoluteLocation location)
+		{
+			GameSession.this.audioManager.blockPlaced(location);
+		}
+		@Override
+		public void blockBroken(AbsoluteLocation location)
+		{
+			GameSession.this.audioManager.blockBroken(location);
+		}
 	}
 }
