f4920162add5b73f03c442a69eccdf400d55f82d
Jeff Disher <jeff@jeffdisher.com>
2025-09-07 17:30:04 -0400

    The Window doesn't need a type binding
    -this seems to have been a hold-over from an earlier approach of UI organization

--- core/src/main/java/com/jeffdisher/october/peaks/ui/Window.java
+++ core/src/main/java/com/jeffdisher/october/peaks/ui/Window.java
@@ -5,7 +5,7 @@ package com.jeffdisher.october.peaks.ui;
  * A window represents a logical root of view-binding coupling which can be selectively rendered into the screen.
  * It is operated upon, from the outside, when rendering the forest of windows in a UI mode.
  */
-public record Window<T>(Rect location
+public record Window(Rect location
 		, IView view
 )
 {
