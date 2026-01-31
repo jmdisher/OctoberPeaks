0445c080c78811655ebb7b56cc79c72260cf950a
Jeff Disher <jeff@jeffdisher.com>
2026-01-30 21:23:18 -0500

    Created new utility for collecting information about exposed burning block faces
    -this isn't yet used but will be used by components which implement various fire animation elements

---
+++ core/src/main/java/com/jeffdisher/october/peaks/scene/FireFaceBuilder.java
@@ -0,0 +1,64 @@
+package com.jeffdisher.october.peaks.scene;
+
+import com.jeffdisher.october.logic.SparseByteCube;
+import com.jeffdisher.october.types.IByteLookup;
+import com.jeffdisher.october.utils.Encoding;
+
+
+/**
+ * Contains the SparseByteArray and bit constants for tracking which faces of a block are visible and on fire.
+ */
+public class FireFaceBuilder
+{
+	public static final byte FACE_NORTH = 0x1;
+	public static final byte FACE_SOUTH = 0x2;
+	public static final byte FACE_EAST = 0x4;
+	public static final byte FACE_WEST = 0x8;
+	public static final byte FACE_UP = 0x10;
+	public static final byte FACE_DOWN = 0x20;
+
+	private final SparseByteCube _fireFaces = new SparseByteCube(Encoding.CUBOID_EDGE_SIZE);
+	private boolean _hasContents;
+
+	/**
+	 * Sets the given bit for the underlying cuboid block location (x, y, z), doing nothing if already set.
+	 * 
+	 * @param x The local x coordinate.
+	 * @param y The local y coordinate.
+	 * @param z The local z coordinate.
+	 * @param bit The bit to set.
+	 */
+	public void setBit(byte x, byte y, byte z, byte bit)
+	{
+		byte bits = _fireFaces.get(x, y, z);
+		
+		// If this is not found, just treat it as an empty bit set.
+		if (IByteLookup.NOT_FOUND == bits)
+		{
+			bits = 0x0;
+		}
+		
+		// Only set the bit if it wasn't set before.
+		if (0x0 == (bits & bit))
+		{
+			bits |= bit;
+			_fireFaces.set(x, y, z, bits);
+			_hasContents = true;
+		}
+	}
+
+	/**
+	 * Extracts the underlying SparseByteCube if any bits were set by the receiver (otherwise, returns null).
+	 * Note that continued interactions with the receiver will have undefined impacts on the returned value after this
+	 * call so no further calls should be made.
+	 * 
+	 * @return The underlying SparseByteCube or null, if nothing was written to it.
+	 */
+	public SparseByteCube extractNonEmptyCollection()
+	{
+		return _hasContents
+			? _fireFaces
+			: null
+		;
+	}
+}
---
+++ core/src/test/java/com/jeffdisher/october/peaks/scene/TestFireFaceBuilder.java
@@ -0,0 +1,48 @@
+package com.jeffdisher.october.peaks.scene;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.jeffdisher.october.logic.SparseByteCube;
+
+
+public class TestFireFaceBuilder
+{
+	@Test
+	public void empty() throws Throwable
+	{
+		FireFaceBuilder builder = new FireFaceBuilder();
+		SparseByteCube cube = builder.extractNonEmptyCollection();
+		Assert.assertNull(cube);
+	}
+
+	@Test
+	public void singleEdges() throws Throwable
+	{
+		FireFaceBuilder builder = new FireFaceBuilder();
+		builder.setBit((byte)1, (byte)2, (byte)3, FireFaceBuilder.FACE_NORTH);
+		builder.setBit((byte)2, (byte)3, (byte)4, FireFaceBuilder.FACE_EAST);
+		builder.setBit((byte)1, (byte)2, (byte)3, FireFaceBuilder.FACE_NORTH);
+		SparseByteCube cube = builder.extractNonEmptyCollection();
+		Assert.assertEquals(FireFaceBuilder.FACE_NORTH, cube.get(1, 2, 3));
+		Assert.assertEquals(FireFaceBuilder.FACE_EAST, cube.get(2, 3, 4));
+	}
+
+	@Test
+	public void multiEdges() throws Throwable
+	{
+		FireFaceBuilder builder = new FireFaceBuilder();
+		builder.setBit((byte)1, (byte)2, (byte)3, FireFaceBuilder.FACE_NORTH);
+		builder.setBit((byte)2, (byte)3, (byte)4, FireFaceBuilder.FACE_EAST);
+		builder.setBit((byte)1, (byte)2, (byte)3, FireFaceBuilder.FACE_SOUTH);
+		builder.setBit((byte)1, (byte)2, (byte)3, FireFaceBuilder.FACE_EAST);
+		builder.setBit((byte)1, (byte)2, (byte)3, FireFaceBuilder.FACE_DOWN);
+		SparseByteCube cube = builder.extractNonEmptyCollection();
+		Assert.assertEquals(FireFaceBuilder.FACE_NORTH
+			| FireFaceBuilder.FACE_SOUTH
+			| FireFaceBuilder.FACE_EAST
+			| FireFaceBuilder.FACE_DOWN
+		, cube.get(1, 2, 3));
+		Assert.assertEquals(FireFaceBuilder.FACE_EAST, cube.get(2, 3, 4));
+	}
+}
